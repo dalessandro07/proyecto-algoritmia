88e8164629abe38451eabf86be6359b26f3471d4
Alessandro Rios <drios28@outlook.es>
2022-10-25 09:52:53 -0500

    alessandro: agregué las variables del proyecto

--- Proyecto Base/Proyecto/src/cibertec/Tienda.java
+++ Proyecto Base/Proyecto/src/cibertec/Tienda.java
@@ -1,74 +1,76 @@
 package cibertec;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.UIManager;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.border.EmptyBorder;
 
 public class Tienda extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	
-	// Datos mínimos del primer colchón
-	public static String marca0 = "Suavestar";
-	public static double precio0 = 499.0;
-	public static int garantia0 = 7;
-	public static String tamaño0 = "1 1/2 Plaza";
-	public static String material0 = "Tela Tricot Acolchada";
-	
-	// Datos mínimos del segundo colchón
-	public static String marca1 = "Springwall";
-	public static double precio1 = 679.0;
-	public static int garantia = 10;
-	public static String tamaño1 = "2 Plazas";
-	public static String material1 = "Tejido de Punto con Algodón Orgánico";
-	
-	// Datos mínimos del tercer colchón
-	public static String marca2 = "Paraiso";
-	public static double precio2 = 479.0;
-	public static int garantia2 = 5;
-	public static String tamaño2 = "1 1/2 Plazas";
-	public static String material2 = "Tejido de Punto";
-	
-	// Datos mínimos del cuarto colchón
-	public static String marca3 = "Drimer";
-	public static double precio3 = 749.0;
-	public static int garantia3 = 4;
-	public static String tamaño3 = "Queen";
-	public static String material3 = "Jacquard";
-	
-	// Datos mínimos del quinto colchón
-	public static String marca4 = "Cisne";
-	public static double precio4 = 389.0;
-	public static int garantia4 = 2;
-	public static String tamaño4 = "1 1/2 Plazas";
-	public static String material4 = "Tejido de Punto";
-	
+
+	// Datos mínimos del primer producto
+	public static String modelo0 = "Cinza Plus";
+	public static double precio0 = 92.56;
+	public static double ancho0 = 62.0;
+	public static double largo0 = 62.0;
+	public static double espesor0 = 8;
+	public static int contenido0 = 6;
+
+	// Datos mínimos del segundo producto
+	public static String modelo1 = "Luxury";
+	public static double precio1 = 42.77;
+	public static double ancho1 = 60;
+	public static double largo1 = 60;
+	public static double espesor1 = 8.5;
+	public static int contenido1 = 4;
+
+	// Datos mínimos del tercer producto
+	public static String modelo2 = "Austria";
+	public static double precio2 = 52.45;
+	public static double ancho2 = 45;
+	public static double largo2 = 45;
+	public static double espesor2 = 6.5;
+	public static int contenido2 = 12;
+
+	// Datos mínimos del cuarto producto
+	public static String modelo3 = "Yungay Mix";
+	public static double precio3 = 55.89;
+	public static double ancho3 = 80;
+	public static double largo3 = 120;
+	public static double espesor3 = 6.8;
+	public static int contenido3 = 9;
+
+	// Datos mínimos del quinto producto
+	public static String modelo4 = "Thalía";
+	public static double precio4 = 45;
+	public static double ancho4 = 45;
+	public static double largo4 = 11.8;
+	public static double espesor4 = 7.2;
+	public static int contenido4 = 10;
+
 	// Porcentajes de descuento
 	public static double porcentaje1 = 7.5;
 	public static double porcentaje2 = 10.0;
 	public static double porcentaje3 = 12.5;
 	public static double porcentaje4 = 15.0;
-	
-	// Cantidad óptima de colchones vendidos
+
+	// Obsequio
+	public static String tipoObsequio = "Lapicero";
+	public static int obsequioCantidad1 = 2;
+	public static int obsequioCantidad2 = 3;
+	public static int obsequioCantidad3 = 4;
+
+	// Cantidad óptima de unidades vendidas por día
 	public static int cantidadOptima = 10;
-	
-	// Cantidad mínima de colchones adquiridos para acceder al obsequio
-	public static int cantidadMinimaObsequiable = 3;
-		
-	// Obsequio por cantidad mínima de colchones adquiridos
-	public static String obsequio = "USB";
-	
-	// Número de cliente que recibe el premio sorpresa
-	public static int numeroClienteSorpresa = 5;
-	
-	// Premio sorpresa
-	public static String premioSorpresa = "Un polo";
+
+	// Cuota diaria
+	public static double cuotaDiaria = 30000;
 
 	/**
 	 * Launch the application.
@@ -76,8 +78,7 @@ public class Tienda extends JFrame implements ActionListener {
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch (Throwable e) {
+		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 		EventQueue.invokeLater(new Runnable() {
@@ -85,8 +86,7 @@ public class Tienda extends JFrame implements ActionListener {
 				try {
 					Tienda frame = new Tienda();
 					frame.setVisible(true);
-				}
-				catch (Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -107,6 +107,6 @@ public class Tienda extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		
+
 	}
 }
