1644491344bea2eec868ea2b547505b31deedd5f
Mauricio <mauricio.menacho5@gmail.com>
2022-11-24 23:08:47 -0500

    mauricio:Avance Configurar Obsequios

--- ProyectoFinal/Proyecto/src/proyecto/DlgConfigurarObsequios.java
+++ ProyectoFinal/Proyecto/src/proyecto/DlgConfigurarObsequios.java
@@ -4,13 +4,16 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class DlgConfigurarObsequios extends JDialog implements ActionListener {
+public class DlgConfigurarObsequios extends JDialog implements ActionListener, KeyListener {
 	private JTextField txtTipoObsequio;
 	private JTextField txtObsequioCantidad1;
 	private JTextField txtObsequioCantidad2;
@@ -61,6 +64,7 @@ public class DlgConfigurarObsequios extends JDialog implements ActionListener {
 		getContentPane().add(lblObsequioCantidad3);
 
 		txtTipoObsequio = new JTextField();
+		txtTipoObsequio.addKeyListener(this);
 		txtTipoObsequio.setForeground(new Color(0, 0, 0));
 		txtTipoObsequio.setBackground(new Color(255, 255, 255));
 		txtTipoObsequio.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -70,6 +74,7 @@ public class DlgConfigurarObsequios extends JDialog implements ActionListener {
 		txtTipoObsequio.setText(frmPrincipal.tipoObsequio);
 
 		txtObsequioCantidad1 = new JTextField();
+		txtObsequioCantidad1.addKeyListener(this);
 		txtObsequioCantidad1.setBackground(new Color(255, 255, 255));
 		txtObsequioCantidad1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtObsequioCantidad1.setColumns(10);
@@ -78,6 +83,7 @@ public class DlgConfigurarObsequios extends JDialog implements ActionListener {
 		txtObsequioCantidad1.setText("" + frmPrincipal.obsequioCantidad1);
 
 		txtObsequioCantidad2 = new JTextField();
+		txtObsequioCantidad2.addKeyListener(this);
 		txtObsequioCantidad2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtObsequioCantidad2.setColumns(10);
 		txtObsequioCantidad2.setBounds(148, 153, 96, 19);
@@ -85,6 +91,7 @@ public class DlgConfigurarObsequios extends JDialog implements ActionListener {
 		txtObsequioCantidad2.setText("" + frmPrincipal.obsequioCantidad2);
 
 		txtObsequioCantidad3 = new JTextField();
+		txtObsequioCantidad3.addKeyListener(this);
 		txtObsequioCantidad3.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtObsequioCantidad3.setColumns(10);
 		txtObsequioCantidad3.setBounds(148, 217, 96, 19);
@@ -116,17 +123,89 @@ public class DlgConfigurarObsequios extends JDialog implements ActionListener {
 	}
 
 	protected void actionPerformedBtnAceptar(ActionEvent e) {
-		frmPrincipal.tipoObsequio = txtTipoObsequio.getText();
-		frmPrincipal.obsequioCantidad1 = Integer.parseInt(txtObsequioCantidad1.getText());
-		frmPrincipal.obsequioCantidad2 = Integer.parseInt(txtObsequioCantidad2.getText());
-		frmPrincipal.obsequioCantidad3 = Integer.parseInt(txtObsequioCantidad3.getText());
-		dispose();
 
+		dispose();
 	}
 
 	protected void actionPerformedBtnCancelar(ActionEvent e) {
 
 		dispose();
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+		if (e.getSource() == txtObsequioCantidad3) {
+			keyTypedTxtObsequioCantidad3(e);
+		}
+		if (e.getSource() == txtObsequioCantidad2) {
+			keyTypedTxtObsequioCantidad2(e);
+		}
+		if (e.getSource() == txtObsequioCantidad1) {
+			keyTypedTxtObsequioCantidad1(e);
+		}
+		if (e.getSource() == txtTipoObsequio) {
+			keyTypedTxtTipoObsequio(e);
+		}
+	}
+
+	public void keyTypedTxtTipoObsequio(KeyEvent e) {
+
+		char validar = e.getKeyChar();
+
+		if (Character.isDigit(validar)) {
+			getToolkit().beep();
+			e.consume();
+
+			JOptionPane.showMessageDialog(rootPane, "Por favor, ingresar solo letras ¡Gracias!");
+		}
+	}
+
+	private void keyTypedTxtObsequioCantidad1(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		char validar = e.getKeyChar();
+
+		if (Character.isLetter(validar)) {
+			getToolkit().beep();
+			e.consume();
+
+			JOptionPane.showMessageDialog(rootPane, "Por favor, ingresar solo numeros ¡Gracias!");
+		}
+	}
+
+	private void keyTypedTxtObsequioCantidad2(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		char validar = e.getKeyChar();
+
+		if (Character.isLetter(validar)) {
+			getToolkit().beep();
+			e.consume();
+
+			JOptionPane.showMessageDialog(rootPane, "Por favor, ingresar solo numeros ¡Gracias!");
+		}
+	}
+
+	protected void keyTypedTxtObsequioCantidad3(KeyEvent e) {
+		char validar = e.getKeyChar();
+
+		if (Character.isLetter(validar)) {
+			getToolkit().beep();
+			e.consume();
+
+			JOptionPane.showMessageDialog(rootPane, "Por favor, ingresar solo numeros ¡Gracias!");
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 }
--- ProyectoFinal/Proyecto/src/proyecto/DlgVender.java
+++ ProyectoFinal/Proyecto/src/proyecto/DlgVender.java
@@ -1,22 +1,20 @@
 package proyecto;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JTextField;
-import java.awt.Dialog.ModalExclusionType;
-import java.awt.event.ActionListener;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class DlgVender extends JDialog implements ActionListener {
 
@@ -31,7 +29,6 @@ public class DlgVender extends JDialog implements ActionListener {
 	private JTextArea txtS;
 	private JLabel lblCantidad;
 	private JTextField txtCantidad;
-	
 
 	/**
 	 * Launch the application.
@@ -56,27 +53,28 @@ public class DlgVender extends JDialog implements ActionListener {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		lblModelo = new JLabel("Modelo");
 		lblModelo.setBounds(10, 25, 49, 14);
 		contentPanel.add(lblModelo);
-		
+
 		cboModelo = new JComboBox();
 		cboModelo.addActionListener(this);
-		cboModelo.setModel(new DefaultComboBoxModel(new String[] {"Cinza Plus", "Luxury", "Austria", "Yungay Mix", "Thalia"}));
+		cboModelo.setModel(
+				new DefaultComboBoxModel(new String[] { "Cinza Plus", "Luxury", "Austria", "Yungay Mix", "Thalia" }));
 		cboModelo.setBounds(100, 21, 148, 22);
 		contentPanel.add(cboModelo);
-		
+
 		lblPrecio = new JLabel("Precio (S/.)");
 		lblPrecio.setBounds(10, 50, 80, 14);
 		contentPanel.add(lblPrecio);
-		
+
 		txtPrecio = new JTextField();
 		txtPrecio.setEditable(false);
 		txtPrecio.setBounds(100, 47, 148, 20);
 		contentPanel.add(txtPrecio);
 		txtPrecio.setColumns(10);
-		
+
 		btnProcesar = new JButton("Procesar");
 		btnProcesar.addActionListener(this);
 		btnProcesar.setBounds(325, 21, 89, 23);
@@ -93,6 +91,7 @@ public class DlgVender extends JDialog implements ActionListener {
 			contentPanel.add(scrollPane);
 			{
 				txtS = new JTextArea();
+				txtS.setFont(new Font("Monospaced", Font.PLAIN, 15));
 				scrollPane.setViewportView(txtS);
 			}
 		}
@@ -108,6 +107,7 @@ public class DlgVender extends JDialog implements ActionListener {
 			txtCantidad.setColumns(10);
 		}
 	}
+
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == btnBorrar) {
 			actionPerformedBtnBorrar(e);
@@ -119,74 +119,88 @@ public class DlgVender extends JDialog implements ActionListener {
 			actionPerformedcbo(e);
 		}
 	}
-	
 
 	private void actionPerformedcbo(ActionEvent e) {
 		int modelo;
-		//entrada de datos
+		// entrada de datos
 		modelo = cboModelo.getSelectedIndex();
-		//mostrar datos de acuerdo al modeo
-		if(modelo==0) {// Cinza Plus
-			txtPrecio.setText(""+frmPrincipal.precio0);
-			
-			
+		// mostrar datos de acuerdo al modeo
+		if (modelo == 0) {// Cinza Plus
+			txtPrecio.setText("" + frmPrincipal.precio0);
+
 		}
-		if(modelo == 1) {
-			txtPrecio.setText(""+frmPrincipal.precio1);
-			
+		if (modelo == 1) {
+			txtPrecio.setText("" + frmPrincipal.precio1);
+
 		}
-		if(modelo == 2) {
-			txtPrecio.setText(""+frmPrincipal.precio2);
-		
+		if (modelo == 2) {
+			txtPrecio.setText("" + frmPrincipal.precio2);
+
 		}
-		if(modelo == 3) {
-			txtPrecio.setText(""+frmPrincipal.precio3);
-		
+		if (modelo == 3) {
+			txtPrecio.setText("" + frmPrincipal.precio3);
+
 		}
-		if(modelo == 4) {
-			txtPrecio.setText(""+frmPrincipal.precio4);
-		
+		if (modelo == 4) {
+			txtPrecio.setText("" + frmPrincipal.precio4);
+
 		}
 	}
 
 	protected void actionPerformedBtnProcesar(ActionEvent e) {
 		int cantidad, canobsequio;
 		double impD, impC, impP, precio;
-		String modelo1,tipo;
+		String modelo1, tipo;
 		cantidad = Integer.parseInt(txtCantidad.getText());
 		modelo1 = cboModelo.getSelectedItem().toString();
 		precio = Double.parseDouble(txtPrecio.getText());
 		tipo = frmPrincipal.tipoObsequio;
-		//Calculando el importe de compra
-		switch(cboModelo.getSelectedIndex()) {
-		case 0: impC = cantidad*frmPrincipal.precio0; break;
-		case 1: impC = cantidad*frmPrincipal.precio1; break;
-		case 2: impC = cantidad*frmPrincipal.precio2; break;
-		case 3: impC = cantidad*frmPrincipal.precio3; break;
-		default: impC = cantidad*frmPrincipal.precio4; break;
+		// Calculando el importe de compra
+		switch (cboModelo.getSelectedIndex()) {
+		case 0:
+			impC = cantidad * frmPrincipal.precio0;
+			break;
+		case 1:
+			impC = cantidad * frmPrincipal.precio1;
+			break;
+		case 2:
+			impC = cantidad * frmPrincipal.precio2;
+			break;
+		case 3:
+			impC = cantidad * frmPrincipal.precio3;
+			break;
+		default:
+			impC = cantidad * frmPrincipal.precio4;
+			break;
 		}
-		//Calculando el importe de descuento
-		if(cantidad<=5) impD=impC/100*frmPrincipal.porcentaje1;
-		else if(cantidad<=10) impD=impC/100*frmPrincipal.porcentaje2;
-		else if(cantidad<=15) impD=impC/100*frmPrincipal.porcentaje3;
-		else impD=impC/100*frmPrincipal.porcentaje4;
+		// Calculando el importe de descuento
+		if (cantidad <= 5)
+			impD = impC / 100 * frmPrincipal.porcentaje1;
+		else if (cantidad <= 10)
+			impD = impC / 100 * frmPrincipal.porcentaje2;
+		else if (cantidad <= 15)
+			impD = impC / 100 * frmPrincipal.porcentaje3;
+		else
+			impD = impC / 100 * frmPrincipal.porcentaje4;
 		impP = impC - impD;
-		if(cantidad<=5) canobsequio = cantidad*frmPrincipal.obsequioCantidad1;
-		else if(cantidad<=10) canobsequio = cantidad*frmPrincipal.obsequioCantidad2; 
-		else canobsequio = cantidad*frmPrincipal.obsequioCantidad3;
+		if (cantidad <= 5)
+			canobsequio = cantidad * frmPrincipal.obsequioCantidad1;
+		else if (cantidad <= 10)
+			canobsequio = cantidad * frmPrincipal.obsequioCantidad2;
+		else
+			canobsequio = cantidad * frmPrincipal.obsequioCantidad3;
 		txtS.setText("Boleta de Venta \n\n");
-		txtS.append("Modelo\t\t: "+ modelo1+"\n");
-		txtS.append("Precio\t\t: S/."+ precio+"\n");
-		txtS.append("Cantidad Adquirida\t: "+ cantidad+"\n");
-		txtS.append("Importe de Compra\t: S/." +impC+"\n");
-		txtS.append("Importe de Descuento\t: S/."+ impD+"\n");
-		txtS.append("Importe de Descuento\t: S/."+ impP+"\n");
-		txtS.append("Tipo de obsequio\t: "+ tipo+"\n");
-		txtS.append("Unidades obsequiadas\t: "+ canobsequio+"\n");
-		
-		
-	
+		txtS.append("Modelo\t\t: " + modelo1 + "\n");
+		txtS.append("Precio\t\t: S/." + precio + "\n");
+		txtS.append("Cantidad Adquirida\t: " + cantidad + "\n");
+		txtS.append("Importe de Compra\t: S/." + impC + "\n");
+		txtS.append("Importe de Descuento\t: S/." + impD + "\n");
+		txtS.append("Importe de Descuento\t: S/." + impP + "\n");
+		txtS.append("Tipo de obsequio\t: " + tipo + "\n");
+		txtS.append("Unidades obsequiadas\t: " + canobsequio + "\n");
+
 	}
+
 	protected void actionPerformedBtnBorrar(ActionEvent e) {
 		txtCantidad.setText("");
 		txtS.setText("");
